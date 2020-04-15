9fea650cf75c9256bf397413ad8abc46ab5e887b
MohamedElshadid <elshadid_mohamed@yahoo.com>
2020-04-15 10:40:14 +0200

    UI Design

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  	root 'pages#home'
-  	get 'about' => 'pages#about'
+  as :user do
+  	# Customize root for user 
+  	get "signin" => 'devise/sessions#new'
+  	delete "signout" => 'devise/sessions#destroy'
+  	get 'signup' => 'devise/registrations#new'
+  end
+  root 'pages#home'
+  get 'about' => 'pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
